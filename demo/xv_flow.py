e627628288e3278276db4c018bf9c37be1abb211
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-03-10 17:55:13 -0400

    Update xv_flow.py

--- demo/xv_flow.py
+++ demo/xv_flow.py
@@ -7,3 +7,10 @@ def flow_to_flo(flow_uv, filename):
         f.write('PIEH'.encode('ascii'))
         f.write(struct.pack('<II', flow_uv.shape[1], flow_uv.shape[0]))
         f.write(flow_uv.astype(np.float32).tobytes())
+
+def flo_to_flow(filename):
+    with open(filename, 'rb') as f:
+        f.read(4)
+        width, height = struct.unpack('<II', f.read(8))
+        return np.frombuffer(f.read(), dtype=np.float32).reshape((height, width, 2))
+    
