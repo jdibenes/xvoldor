82cfd1aa54747fb1f21156c4250315b95cae8204
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-03-10 17:55:10 -0400

    Update pose_cmp.py

--- demo/pose_cmp.py
+++ demo/pose_cmp.py
@@ -1,23 +1,76 @@
 
 import numpy as np
 import kitti
+import os
 
-fname_gt = './data/KITTI-03/03.txt'
-fname_cmp = './poses/pose_KITTI-03_stereo_searaft.txt'
+def do_cmp(mode, toolname):
+    bigtable = [
+        ('KITTI-00', '00.txt'),
+        ('KITTI-01', '01.txt'),
+        ('KITTI-02', '02.txt'),
+        ('KITTI-03', '03.txt'),
+        ('KITTI-04', '04.txt'),
+        ('KITTI-05', '05.txt'),
+        ('KITTI-06', '06.txt'),
+        ('KITTI-07', '07.txt'),
+        ('KITTI-08', '08.txt'),
+        ('KITTI-09', '09.txt'),
+        ('KITTI-10', '10.txt'),
+    ]
 
-poses_gt_obj = kitti.pose_loader(fname_gt)
-poses_cmp_obj = kitti.pose_loader(fname_cmp)
+    err = []
 
-err_obj = kitti.calcSequenceErrors(poses_gt_obj, poses_cmp_obj)
-err_r_all = 0
-err_t_all = 0
-for _, err_r, err_t, _, _ in err_obj:
-    err_r_all += err_r
-    err_t_all += err_t
+    for sequence, sequence_gt in bigtable:
+        fname_gt = os.path.join('./data', sequence, sequence_gt)
+        fname_cmp = os.path.join('./poses', f'pose_{sequence}_{mode}_{toolname}.txt')
 
-#print(err_obj)
-print(err_r_all / len(err_obj))
-print(err_t_all / len(err_obj))
+        poses_gt_obj = kitti.pose_loader(fname_gt)
+        poses_cmp_obj = kitti.pose_loader(fname_cmp)
+
+        err_obj = kitti.calcSequenceErrors(poses_gt_obj, poses_cmp_obj)
+
+        err_r_all = 0
+        err_t_all = 0
+        for _, err_r, err_t, _, _ in err_obj:
+            err_r_all += err_r
+            err_t_all += err_t
+        err_r = err_r_all / len(err_obj)
+        err_t = err_t_all / len(err_obj)
+
+        #err = np.array([err_r, err_t], dtype=np.float64)
+        err.append([err_r, err_t])
+
+    fname_out = os.path.join('./errors', f'err_{mode}_{toolname}.txt')
+    np.savetxt(fname_out, np.array(err, dtype=np.float64).reshape((-1, 2)), delimiter=' ')
+    #np.array(err, dtype=np.float64).reshape((-1, 2)).tofile(fname_out, sep=' ')
+    #print(err)
+
+for n_mode in ['stereo', 'mono-scaled', 'mono']:
+    print(n_mode)
+    for n_tool in ['ptl-maskflownet', 'ptl-memflow', 'ptl-neuflow2', 'searaft', 'ptl-pwcnet']:
+        do_cmp(n_mode, n_tool)
+
+
+
+
+
+
+#fname_gt = './data/KITTI-03/03.txt'
+#fname_cmp = './poses/pose_KITTI-03_stereo_searaft.txt'
+
+#poses_gt_obj = kitti.pose_loader(fname_gt)
+#poses_cmp_obj = kitti.pose_loader(fname_cmp)
+
+#err_obj = kitti.calcSequenceErrors(poses_gt_obj, poses_cmp_obj)
+#err_r_all = 0
+#err_t_all = 0
+#for _, err_r, err_t, _, _ in err_obj:
+#    err_r_all += err_r
+#    err_t_all += err_t
+
+##print(err_obj)
+#print(err_r_all / len(err_obj))
+#print(err_t_all / len(err_obj))
 
 ''' 
 KITTI-05
