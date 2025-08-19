7095a172cbfc6fe339535b62823d811a2dbaf1c8
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-08-19 17:26:01 -0400

    Update geometry.h

--- voldor/geometry.h
+++ voldor/geometry.h
@@ -2,12 +2,21 @@
 #include "utils.h"
 #include "config.h"
 
-int optimize_camera_pose(std::vector<cv::Mat> flows, std::vector<cv::Mat> rigidnesses, 
-	cv::Mat depth, std::vector<Camera>& cams,
-	int n_flows, int active_idx, bool successive_pose,
+int 
+optimize_camera_pose
+(
+	std::vector<cv::Mat> const& flows,
+	std::vector<cv::Mat> const& rigidnesses, 
+	cv::Mat const& depth,
+	std::vector<Camera>& cams,
+	int n_flows,
+	int active_idx,
+	bool successive_pose,
 	bool rg_refine,
-	bool update_batch_instance, bool update_iter_instance,
-	Config cfg);
+	bool update_batch_instance,
+	bool update_iter_instance,
+	Config const& cfg
+);
 
 void estimate_depth_closed_form(cv::Mat flow, cv::Mat& depth, Camera cam,
 	float min_depth = 1e-2f, float max_depth = 1e10f);
