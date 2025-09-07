85183230858bacf4b376fa10382405c73fa76fa9
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-09-06 21:14:33 -0400

    Update trifocal.h

--- voldor/trifocal.h
+++ voldor/trifocal.h
@@ -2,3 +2,5 @@
 #pragma once
 
 void trifocal_R_t(float const* p2d_1, float const* p2d_2, float const* p2d_3, float const* sp2d, float const* sp3d, float* tft, float* r1, float* t1, float* r2, float* t2, float* s1, float* s2);
+void trifocal_rng_initialize(int count, int* seeds);
+int trifocal_R_t_batch(int jobs, int workers, float const* p2d_1, float const* p2d_2, float const* p2d_3, float const* sp2d, float const* sp3d, int count, float* tft, float* r1, float* t1, float* r2, float* t2, float* s1, float* s2);
