25a6d9ace4f5a8408ab294cde75f54c6d56b7b93
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-11-27 02:29:02 -0500

    Update py_export.h

--- voldor/py_export.h
+++ voldor/py_export.h
@@ -1,15 +1,33 @@
+
 #pragma once
 
-extern int py_voldor_wrapper(
+extern
+int
+py_voldor_wrapper
+(
 	// inputs
-	const float* flows, const float* disparity, const float* disparity_pconf,
-	const float* depth_priors, const float* depth_prior_poses, const float* depth_prior_pconfs,
-	const float fx, const float fy, const float cx, const float cy, const float basefocal,
-	const int N, const int N_dp, const int w, const int h,
-	const char* config,
+	float const* flows_1_pt,
+	float const* flows_2_pt,
+	float const* disparities_pt,
+	float const* disparity,
+	float const* disparity_pconf,
+	float const* depth_priors,
+	float const* depth_prior_poses,
+	float const* depth_prior_pconfs,
+	float const fx,
+	float const fy,
+	float const cx,
+	float const cy,
+	float const basefocal,
+	int const N,
+	int const N_dp,
+	int const w,
+	int const h,
+	char const* config,
 	// outputs
-	int& n_registered, float* poses, float* poses_covar, float* depth, float* depth_conf,
-	// extension
-	// inputs
-	const float* flows_2,
-	const float* disparities_pt);
+	int& n_registered,
+	float* poses,
+	float* poses_covar,
+	float* depth,
+	float* depth_conf
+);
