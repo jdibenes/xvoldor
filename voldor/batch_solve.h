734fea04306edb20d2f3968867bc479f00720389
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-11-29 03:32:06 -0500

    Update batch_solve.h

--- voldor/batch_solve.h
+++ voldor/batch_solve.h
@@ -7,7 +7,4 @@ int batch_solve_ap3p_cpu(std::vector<cv::Point2f> const& pts2, std::vector<cv::P
 int batch_solve_lambdatwist_cpu(std::vector<cv::Point2f> const& pts2, std::vector<cv::Point3f> const& pts3, cv::Mat const& K, int poses_to_sample, cv::Mat& poses_pool);
 int batch_solve_ap3p_gpu(std::vector<cv::Point2f> const& pts2, std::vector<cv::Point3f> const& pts3, cv::Mat const& K, int poses_to_sample, cv::Mat& poses_pool);
 int batch_solve_lambdatwist_gpu(std::vector<cv::Point2f> const& pts2, std::vector<cv::Point3f> const& pts3, cv::Mat const& K, int poses_to_sample, cv::Mat& poses_pool);
-int batch_solve_gpm_hpc0_cpu(std::vector<cv::Point3f> const& pts0, std::vector<cv::Point3f> const& pts1,
-	//std::vector<cv::Point3f> pts3,
-	//std::vector<cv::Point2f> pts2,
-	cv::Mat const& K, int poses_to_sample, cv::Mat& poses_pool);
+int batch_solve_gpm_hpc0_cpu(std::vector<cv::Point3f> const& pts0, std::vector<cv::Point3f> const& pts1, cv::Mat const& K, int poses_to_sample, cv::Mat& poses_pool);
