02d0a46154e7e5cdf0f93e8616532becf567bfc3
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-11-29 01:24:43 -0500

    Update pyvoldor_full.pyx

--- slam_py/install/pyvoldor_full.pyx
+++ slam_py/install/pyvoldor_full.pyx
@@ -4,18 +4,15 @@ from libcpp cimport bool
 
 cdef extern from "../../voldor/py_export.h":
     int py_voldor_wrapper(
-        const float* flows, const float* disparity, const float* disparity_pconf,
+        const float* flows, const float* flows_2, const float* disparities, const float* disparity, const float* disparity_pconf,
         const float* depth_priors, const float* depth_prior_poses, const float* depth_prior_pconfs,
         const float fx, const float fy, const float cx, const float cy, const float basefocal,
         const int N, const int N_dp, const int w, const int h,
         const char* config,
-        int& n_registered, float* poses, float* poses_covar, float* depth, float* depth_conf,
-        const float* flows_2,
-        const float* disparities)
+        int& n_registered, float* poses, float* poses_covar, float* depth, float* depth_conf)
 
 def voldor(
     np.ndarray[float, ndim=4] flows not None,
-    np.ndarray[float, ndim=4] flows_2 not None,
     float fx, float fy, float cx, float cy, 
     float basefocal = 0,
     np.ndarray[float, ndim=2] disparity = None,
@@ -23,6 +20,7 @@ def voldor(
     np.ndarray[float, ndim=3] depth_priors = None,
     np.ndarray[float, ndim=2] depth_prior_poses = None,
     np.ndarray[float, ndim=3] depth_prior_pconfs = None,
+    np.ndarray[float, ndim=4] flows_2 = None,
     np.ndarray[float, ndim=3] disparities = None,
     str config=''):
     
@@ -32,7 +30,10 @@ def voldor(
     cdef int N_dp = 0 if depth_priors is None else depth_priors.shape[0]
 
     flows = np.ascontiguousarray(flows)
-    flows_2 = np.ascontiguousarray(flows_2)
+    if flows_2 is not None:
+        flows_2 = np.ascontiguousarray(flows_2)
+    if disparities is not None:
+        disparities = np.ascontiguousarray(disparities)
     if disparity is not None:
         disparity = np.ascontiguousarray(disparity)
     if depth_priors is not None:
@@ -41,8 +42,6 @@ def voldor(
         depth_prior_poses = np.ascontiguousarray(depth_prior_poses)
     if depth_prior_pconfs is not None:
         depth_prior_pconfs = np.ascontiguousarray(depth_prior_pconfs)
-    if disparities is not None:
-        disparities = np.ascontiguousarray(disparities)
     
     cdef np.ndarray[float, ndim=2, mode='c'] poses = \
         np.ascontiguousarray(np.zeros((N, 6), dtype=np.float32))
@@ -56,6 +55,8 @@ def voldor(
     cdef int n_registered = 0
     py_voldor_wrapper(
                 &flows[0,0,0,0],
+                &flows_2[0,0,0,0],
+                &disparities[0,0,0] if disparities is not None else NULL,
                 &disparity[0,0] if disparity is not None else NULL,
                 &disparity_pconf[0,0] if disparity_pconf is not None else NULL,
                 &depth_priors[0,0,0] if depth_priors is not None else NULL,
@@ -68,9 +69,7 @@ def voldor(
                 &poses[0,0],
                 &poses_covar[0,0,0],
                 &depth[0,0],
-                &depth_conf[0,0],
-                &flows_2[0,0,0,0],
-                &disparities[0,0,0] if disparities is not None else NULL)
+                &depth_conf[0,0])
 
     return {'n_registered': n_registered,
             'poses': poses[:n_registered],
