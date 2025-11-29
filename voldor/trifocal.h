69e5ff02bee489f522599034acbfb4473992122b
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-11-29 18:10:02 -0500

    Update trifocal.h

--- voldor/trifocal.h
+++ voldor/trifocal.h
@@ -1,5 +1,20 @@
 
 #pragma once
 
+bool
+trifocal_R_t_linear
+(
+    float const* p2d_1,
+    float const* p2d_2,
+    float const* p2d_3,
+    float const* p3d_1,
+    int N,
+    bool use_prior,
+    float* r1,
+    float* t1,
+    float* r2,
+    float* t2
+);
+
 bool trifocal_R_t(float const* p2d_1, float const* p2d_2, float const* p2d_3, float const* sp2d, float const* sp3d, float* tft, float* rt1, float* rt2, float* s1, float* s2);
 int trifocal_R_t_batch(int jobs, int workers, float const* p2d_1, float const* p2d_2, float const* p2d_3, float const* p2d_s, float const* p3d_s, int count, float* tft, float* rt1, float* rt2, float* s1, float* s2);
