50d723f30469f6605f5cc8d61fc303b34c043d11
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-11-29 18:09:43 -0500

    Update batch_solve.h

--- voldor/batch_solve.h
+++ voldor/batch_solve.h
@@ -8,3 +8,5 @@ int batch_solve_lambdatwist_cpu(std::vector<cv::Point2f> const& pts2, std::vecto
 int batch_solve_ap3p_gpu(std::vector<cv::Point2f> const& pts2, std::vector<cv::Point3f> const& pts3, cv::Mat const& K, int poses_to_sample, cv::Mat& poses_pool);
 int batch_solve_lambdatwist_gpu(std::vector<cv::Point2f> const& pts2, std::vector<cv::Point3f> const& pts3, cv::Mat const& K, int poses_to_sample, cv::Mat& poses_pool);
 int batch_solve_gpm_hpc0_cpu(std::vector<cv::Point3f> const& pts0, std::vector<cv::Point3f> const& pts1, cv::Mat const& K, int poses_to_sample, cv::Mat& poses_pool);
+int batch_solve_tft_linear_cpu(std::vector<cv::Point3f> const& pts0, std::vector<cv::Point3f> const& pts1, std::vector<cv::Point3f> const& pts2, cv::Mat const& K, int poses_to_sample, cv::Mat& poses_pool);
+
