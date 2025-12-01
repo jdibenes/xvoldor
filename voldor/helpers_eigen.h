f2e360dc2bb3e71591ce9a22092d34fe872a3be5
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-12-01 03:24:52 -0500

    Update helpers_eigen.h

--- voldor/helpers_eigen.h
+++ voldor/helpers_eigen.h
@@ -18,3 +18,46 @@ void matrix_to_buffer(Eigen::Matrix<_scalar, _rows, _cols> const& M, _scalar* da
     Eigen::Index cols = M.cols();
     memcpy(data, M.data(), sizeof(_scalar) * rows * cols);
 }
+
+template <typename _scalar, int _rows_1, int _cols_1, int _rows_2, int _cols_2>
+Eigen::Matrix<_scalar, Eigen::Dynamic, Eigen::Dynamic> vector_convolve(Eigen::Matrix<_scalar, _rows_1, _cols_1> const& o1, Eigen::Matrix<_scalar, _rows_2, _cols_2> const& o2)
+{
+    Eigen::Index sz1 = o1.size();
+    Eigen::Index sz2 = o2.size();
+
+    Eigen::Matrix<_scalar, Eigen::Dynamic, Eigen::Dynamic> rv = Eigen::Matrix<_scalar, Eigen::Dynamic, Eigen::Dynamic>::Zero(sz1 + sz2 - 1, 1);
+
+    _scalar* pr = rv.data();
+
+    _scalar const* p1 = o1.data();
+    _scalar const* p2 = o2.data();
+
+    for (int i1 = 0; i1 < sz1; ++i1)
+    {
+    for (int i2 = 0; i2 < sz2; ++i2)
+    {
+    pr[i1 + i2] += p2[i2] * p1[i1];
+    }
+    }
+
+    return rv;
+}
+
+template <typename _scalar, int _rows_1, int _cols_1, int _rows_2, int _cols_2>
+Eigen::Matrix<_scalar, Eigen::Dynamic, Eigen::Dynamic> vector_add_padded(Eigen::Matrix<_scalar, _rows_1, _cols_1> const& o1, Eigen::Matrix<_scalar, _rows_2, _cols_2> const& o2, _scalar s1 = 1, _scalar s2 = 1)
+{
+    Eigen::Index sz1 = o1.size();
+    Eigen::Index sz2 = o2.size();
+
+    Eigen::Matrix<_scalar, Eigen::Dynamic, Eigen::Dynamic> rv = Eigen::Matrix<_scalar, Eigen::Dynamic, Eigen::Dynamic>::Zero(std::max(sz1, sz2), 1);
+
+    _scalar* pr = rv.data();
+
+    _scalar const* p1 = o1.data();
+    _scalar const* p2 = o2.data();
+
+    for (int i1 = 0; i1 < sz1; ++i1) { pr[i1] += s1 * p1[i1]; }
+    for (int i2 = 0; i2 < sz2; ++i2) { pr[i2] += s2 * p2[i2]; }
+
+    return rv;
+}
