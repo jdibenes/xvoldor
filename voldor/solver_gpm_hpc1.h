5d0a74d4001d8a363b6eafcd733938bc4673e4b3
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-12-02 02:10:52 -0500

    Update solver_gpm_hpc1.h

--- voldor/solver_gpm_hpc1.h
+++ voldor/solver_gpm_hpc1.h
@@ -1,4 +1,4 @@
 
 #pragma once
 
-bool solver_gpm_hpc1(float const* pa1, float const* pb1, float const* pa2, float const* pb2, float* r01, float* t01, int refine_iterations = 2);
+bool solver_gpm_hpc1(float const* pa1, float const* pb1, float const* pa2, float const* pb2, float* r01, float* t01, int refine_iterations);
