59e8eaf5d8ea5bcf3b812833c34017bc8e3444b0
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-01-12 12:53:48 -0500

    Create solver_tft_linear.cpp

---
+++ voldor/solver_tft_linear.cpp
@@ -0,0 +1,23 @@
+
+#include "trifocal.h"
+
+
+bool
+trifocal_R_t_linear
+(
+    float const* p2d_1,
+    float const* p2d_2,
+    float const* p2d_3,
+    float const* p3d_1,
+    int N,
+    bool use_prior,
+    float* r1,
+    float* t1,
+    float* r2,
+    float* t2
+);
+
+void f()
+{
+
+}
