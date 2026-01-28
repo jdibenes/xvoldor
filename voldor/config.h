e34156e293e18b0f78b9ac899ce074fd4c4e9f62
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-01-27 19:39:51 -0500

    Update config.h

--- voldor/config.h
+++ voldor/config.h
@@ -6,8 +6,8 @@ struct Config
 {
 	//vector<string> flow_names;
 	int multiview_mode = 2; // 2: binocular, 3: trifocal
-	int solver_select = 4;
-	int disparities_enable = 1;
+	int solver_select = 3;
+	int disparities_enable = 0;
 	int trifocal_enable_flow_2 = 0;
 	int root_refine_interations = 2;
 
