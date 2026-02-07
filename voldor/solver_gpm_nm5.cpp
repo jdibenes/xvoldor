fa2683f6629f721f22280d626ac9adefc00ceaf2
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-02-07 02:21:11 -0500

    Create solver_gpm_nm5.cpp

---
+++ voldor/solver_gpm_nm5.cpp
@@ -0,0 +1,78 @@
+
+#include <Eigen/Eigen>
+#include "polynomial.h"
+#include "helpers_eigen.h"
+#include "helpers_geometry.h"
+
+bool solver_gpm_nm5(float const* p1, float const* p2, float* r01, float* t01)
+{
+    Eigen::Matrix<float, 3, 5> P1 = matrix_from_buffer<float, 3, 5>(p1);
+    Eigen::Matrix<float, 3, 5> P2 = matrix_from_buffer<float, 3, 5>(p2);
+
+    Eigen::Matrix<float, 2, 5> q1 = P1.colwise().hnormalized();
+    Eigen::Matrix<float, 2, 5> q2 = P2.colwise().hnormalized();
+
+    Eigen::Matrix<float, 3, 5> Q1 = q1.colwise().homogeneous();
+    Eigen::Matrix<float, 3, 5> Q2 = q2.colwise().homogeneous();
+
+    Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> Q = matrix_E_constraints(Q1, Q2);
+
+    Q.col(4) = Q.col(4) - Q.col(0);
+    Q.col(8) = Q.col(8) - Q.col(0);
+
+    Eigen::Matrix<float, 8, 3> k = Q(Eigen::all, Eigen::seqN(1, 8)).fullPivLu().kernel();
+    Eigen::Matrix<float, 9, 3> e;
+
+    e << (-(k(3, Eigen::all) + k(7, Eigen::all))), k;
+
+    Eigen::Matrix<polynomial<float, 2>, 3, 3> E = matrix_to_linear_polynomial_matrix<float, 2, 3, 3>(e); // OK
+
+    polynomial<float, 2> E_determinant = E.determinant();
+
+    Eigen::Matrix<polynomial<float, 2>, 3, 3> EEt = E * E.transpose();
+    Eigen::Matrix<polynomial<float, 2>, 3, 3> E_singular_values = (EEt * E) - ((0.5 * EEt.trace()) * E);
+
+    Eigen::Matrix<float, 10, 10> S = Eigen::Matrix<float, 10, 10>::Zero();
+
+    int const flatten[4][4] =
+    {
+        { 0,  4,  7,  9 }, // 1,      y,  y^2, y^3
+        { 1,  5,  8, -1 }, // x,     xy, xy^2,
+        { 2,  6, -1, -1 }, // x^2, x^2y,
+        { 3, -1, -1, -1 }, // x^3,
+    };
+
+    for (int i = 0; i < 3; ++i)
+    {
+    for (int j = 0; j < 3; ++j)
+    {
+    E_singular_values(j, i).for_each([&](float const& element, polynomial<float, 1>::indices_t const& indices) { if (flatten[indices[0]][indices[1]] >= 0) { S((i * 3) + j, flatten[indices[0]][indices[1]]) = element; } });
+    }
+    }
+
+    E_determinant.for_each([&](float const& element, polynomial<float, 1>::indices_t const& indices) { if (flatten[indices[0]][indices[1]] >= 0) { S(9, flatten[indices[0]][indices[1]]) = element; } });
+    
+    Eigen::Matrix<float, 10, 1> solution = S.bdcSvd(Eigen::ComputeThinV).matrixV().col(9);
+
+    Eigen::Matrix<float, 3, 3> fake_E = (e(Eigen::all, 0) + ((solution(1) / solution(0)) * e(Eigen::all, 1)) + ((solution(4) / solution(0)) * e(Eigen::all, 2))).reshaped(3, 3);
+
+    result_R_t_from_E result = R_t_from_E(fake_E, q1, q2);
+
+    Eigen::Matrix<float, 3, 3> R = result.P(Eigen::all, Eigen::seqN(0, 3));
+    Eigen::Matrix<float, 3, 1> v = result.P.col(3);
+
+    Eigen::AngleAxis<float> aa(R);
+
+    Eigen::Matrix<float, 3, 1> r = aa.axis() * aa.angle();
+    Eigen::Matrix<float, 3, 1> t = (P2.col(0) - R * P1.col(0)).norm() * v;
+
+    matrix_to_buffer(r, r01);
+    matrix_to_buffer(t, t01);
+
+    float r_sum = r01[0] + r01[1] + r01[2];
+    float t_sum = t01[0] + t01[1] + t01[2];
+
+    float x_sum = r_sum + t_sum;
+
+    return std::isfinite(x_sum);
+}
