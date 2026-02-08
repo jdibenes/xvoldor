439cb4c4cffc646070573c5f8b3516f66b84981d
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-02-07 19:55:21 -0500

    unravel grevlex

--- voldor/polynomial.h
+++ voldor/polynomial.h
@@ -3,6 +3,7 @@
 
 #include <Eigen/Eigen>
 #include <vector>
+#include <iostream>
 #include <type_traits>
 #include "helpers_traits.h"
 
@@ -208,6 +209,170 @@ public:
         return *this;
     }
 };
+
+
+template <int _n>
+class grevlex_generator
+{
+private:
+    std::vector<int> indices = std::vector<int>(_n);
+    int power = -1;
+    int sum = 0;
+
+public:
+    std::vector<int> next()
+    {
+        do
+        {
+            if (sum > 0)
+            {
+                int i;
+                for (i = _n - 1; (i >= 0) && (indices[i] <= 0); --i) { sum += (indices[i] = power); }
+                indices[i]--;
+                sum--;
+            }
+            else
+            {
+                indices[0] = sum = ++power;
+            }
+        } 
+        while (sum != power);
+        return indices;
+    }
+};
+
+
+
+
+
+
+
+
+
+/*
+template <int _n>
+void unravel_grevlex(int index, std::vector<int>& indices, int power = 0)
+{
+    indices[0] = power;
+    for (int i = 1; i < _n; ++i) { indices[i] = 0; }
+
+    while (index >= 0)
+    {
+        int sum = 0;
+        for (int i = 0; i < _n; ++i) { sum += indices[i]; }
+        if ((sum == power) && ((--index) < 0)) { return; }
+        if (sum == 0)
+        {
+            indices[0] = ++power;
+            continue;
+        }
+
+
+
+
+
+
+
+
+        
+        
+        for (int i = _n - 1; i >= 0; --i)
+        {
+            if (indices[i] > 0)
+            {
+                indices[i]--;
+                break;
+            }
+            else
+            {
+                indices[i] = power;
+            }
+        }
+    }
+}
+*/
+
+/*
+for 3 variables
+ 0 [0,0,0] 1
+
+ 1 [1,0,0] x
+ 2 [0,1,0] y
+ 3 [0,0,1] z
+
+ 4 [2,0,0] x^2
+ 5 [1,1,0] xy
+ 6 [1,0,1] xz
+ 7 [0,2,0] y^2
+ 8 [0,1,1] yz
+ 9 [0,0,2] z^2
+
+10 [3,0,0] x^3
+11 [2,1,0] x^2y
+12 [2,0,1] x^2z
+13 [1,2,0] xy^2
+14 [1,1,1] xyz
+15 [1,0,2] xz^2
+16 [0,3,0] y^3
+17 [0,2,1] y^2z
+18 [0,1,2] yz^2
+19 [0,0,3] z^3
+
+
+
+[1,1,1]
+[0,3,0]
+[1,2,0]
+[0,2,1]
+[
+
+
+*/
+
+/*
+
+template <int _n, typename A>
+polynomial<typename A::Scalar, _n> vector_polynomial_multivariate(Eigen::MatrixBase<A> const& M)
+{
+    polynomial<typename A::Scalar, _n> rv;
+    typename polynomial<typename A::Scalar, _n>::index_t indices(_n);
+    Eigen::Index cc = std::min({ Eigen::index(_n + 1, M.size() });
+    for (int p = 0; p < cc; ++p)
+    {
+        
+        
+        
+        //for (int q = 0; q < _n; ++q) { indices[q] = p == (q + 1); }
+        rv[indices] = M(p);
+    }
+    return rv;
+}
+
+
+
+
+
+
+template <typename A>
+polynomial<typename A::Scalar, 1> vector_to_polynomial_univariate(Eigen::MatrixBase<A> const& M)
+{
+    polynomial<typename A::Scalar, 1> rv;
+    for (Eigen::index i = 0; i < M.size(); ++i)
+    {
+    rv({ polynomial<typename A::Scalar, 1>::index_t(i) }) = M(i);
+    }
+    return rv;
+}
+*/
+
+
+
+
+
+
+
+
+
 
 template <typename _scalar, int _n, int _output_rows, int _output_cols, typename A>
 Eigen::Matrix<polynomial<_scalar, _n>, _output_rows, _output_cols> matrix_to_linear_polynomial_matrix(Eigen::MatrixBase<A> const& M, int output_rows = _output_rows, int output_cols = _output_cols)
