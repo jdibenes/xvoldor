3248ecf89864ef91f3dcb297a84aa3c030cbf6fa
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-02-26 22:28:35 -0500

    Update solver_gpm_hpc1.cpp

--- voldor/solver_gpm_hpc1.cpp
+++ voldor/solver_gpm_hpc1.cpp
@@ -1,6 +1,6 @@
 
 #include <Eigen/Eigen>
-#include "solver_gpm_hpc0.h"
+#include "solver_gpm.h"
 #include "algebra.h"
 #include "helpers.h"
 #include "helpers_eigen.h"
@@ -26,7 +26,7 @@ bool solver_gpm_hpc1(float const* p1, float const* p2, float* r01, float* t01, i
     float z1 = roots[0];
     float z2 = roots[1];
 
-    Eigen::Matrix<float, 3, 1> PB2_w = PB2_n * ((std::abs(PB2(2, 0) - z1) <= std::abs(PB2(2, 0) - z2)) ? z1 : z2);
+    P2.col(1) = PB2_n * ((std::abs(PB2(2, 0) - z1) <= std::abs(PB2(2, 0) - z2)) ? z1 : z2);
 
-    return solver_gpm_hpc0(PA1.data(), PB1.data(), PA2.data(), PB2_w.data(), r01, t01);
+    return solver_gpm_hpc0(P1.data(), P2.data(), r01, t01);
 }
