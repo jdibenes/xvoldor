d4054bc4b7c772d4eed9965f41d3b404b60b4f32
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-03-07 14:37:53 -0500

    Update helpers.h

--- voldor/helpers.h
+++ voldor/helpers.h
@@ -7,3 +7,10 @@ T clamp(T v, T l, T r)
 {
     return (v <= l) ? l : ((v >= r) ? r : v);
 }
+
+// OK
+template <typename T>
+T acos_small(T x)
+{
+    return 2 * std::atan2(std::sqrt(1 - x), std::sqrt(1 + x));
+}
