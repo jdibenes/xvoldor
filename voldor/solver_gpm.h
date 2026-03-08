d4c4ea75153777071da824a32e00947be7792d5a
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-03-07 19:07:06 -0500

    Update solver_gpm.h

--- voldor/solver_gpm.h
+++ voldor/solver_gpm.h
@@ -1,12 +1,10 @@
 
 #pragma once
 
-bool solver_gpm_hpc0(float const* p1, float const* p2, float* r01, float* t01);
-bool solver_gpm_hpc1(float const* p1, float const* p2, float* r01, float* t01);
-bool solver_gpm_hpc2(float const* p1, float const* p2, float* r01, float* t01);
+bool solver_gpm_hpc0(float const* p3d_1, float const* p3d_2, float* r_12, float* t_12);
+bool solver_gpm_hpc1(float const* p3d_1, float const* p3d_2, float* r_12, float* t_12);
+bool solver_gpm_hpc2(float const* p3d_1, float const* p3d_2, float* r_12, float* t_12);
 
-bool solver_gpm_nm7(float const* p1, float const* p2, float* r01, float* t01);
-bool solver_gpm_nm6(float const* p1, float const* p2, float* r01, float* t01);
-bool solver_gpm_nm5(float const* p1, float const* p2, float* r01, float* t01);
-
-bool solver_gpm_m7(float const* p1, float const* p2, float* r01, float* t01);
+bool solver_gpm_nm5(float const* p3d_1, float const* p2h_2, float* r_12, float* t_12);
+bool solver_gpm_nm6(float const* p3d_1, float const* p2h_2, float* r_12, float* t_12);
+bool solver_gpm_nm7(float const* p3d_1, float const* p2h_2, float* r_12, float* t_12);
