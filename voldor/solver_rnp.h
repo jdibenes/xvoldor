4138f1bf4be08f9f13e021ce49d86b061510bda0
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-03-07 19:12:55 -0500

    Update solver_rnp.h

--- voldor/solver_rnp.h
+++ voldor/solver_rnp.h
@@ -1,7 +1,6 @@
 
 #pragma once
 
-bool solver_r6p1l(float const* p3d, float const* p2d, bool direction, float r0, int max_pow, float* r01, float* t01);
-bool solver_r6p2l(float const* p3d, float const* p2d, bool direction, float r0, float* r01, float* t01);
-
-bool solver_r6pi(float const* p3d, float const* p2d, bool direction, float r0, int max_iterations, float* r01, float* t01);
+bool solver_r6p1l(float const* p3d_1, float const* p2d_2, bool direction, float r0, float* r_12, float* t_12);
+bool solver_r6p2l(float const* p3d_1, float const* p2d_2, bool direction, float r0, float* r_12, float* t_12);
+bool solver_r6p2i(float const* p3d_1, float const* p2d_2, bool direction, float r0, float* r_12, float* t_12, int max_iterations);
