ea256620525a848f5a0ddf59a01c1f6dc5fba112
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-03-07 19:13:13 -0500

    Update solver_rnp_r6pi.cpp

--- voldor/solver_rnp_r6pi.cpp
+++ voldor/solver_rnp_r6pi.cpp
@@ -5,7 +5,7 @@
 #include "helpers_geometry.h"
 
 // OK
-bool solver_r6pi(float const* p3d_1, float const* p2d_2, bool direction, float r0, int max_iterations, float* r_12, float* t_12)
+bool solver_r6p2i(float const* p3d_1, float const* p2d_2, bool direction, float r0, float* r_12, float* t_12, int max_iterations)
 {
 	cv::Mat r_initial;
 	cv::Mat t_initial;
