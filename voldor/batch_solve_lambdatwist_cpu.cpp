08eea3a6ddc4b853bccaeb1125851d64bbd7710c
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-03-16 15:26:58 -0400

    Update batch_solve_lambdatwist_cpu.cpp

--- voldor/batch_solve_lambdatwist_cpu.cpp
+++ voldor/batch_solve_lambdatwist_cpu.cpp
@@ -1,7 +1,89 @@
 
+#include <iostream>
 #include <opencv2/calib3d.hpp>
+#include "batch_solve_cpu.h"
 #include "../lambdatwist/lambdatwist_p4p.h"
 
+struct job_arguments
+{
+	cv::Point2f const* p2d;
+	cv::Point3f const* p3d;
+	float fx;
+	float fy;
+	float cx;
+	float cy;
+	float* out;
+};
+
+static void block_cpu_solver_p4p_lambdatwist(void* data, job_descriptor& jd, std::atomic<int>& valid)
+{
+	job_arguments* ja = static_cast<job_arguments*>(data);
+
+	float R_temp[3][3];
+	float t_temp[3];
+
+	cv::Vec3f rvec_temp;
+	cv::Vec3f tvec_temp;
+
+	//std::cout << "JOB: " << jd.id << "(" << jd.start << "->" << jd.end << ") : " << jd.seed << std::endl;
+
+	for (int i = jd.start; i < jd.end; ++i)
+	{
+		int i1 = jd.rng[(i * jd.sample_size) + 0];
+		int i2 = jd.rng[(i * jd.sample_size) + 1];
+		int i3 = jd.rng[(i * jd.sample_size) + 2];
+		int i4 = jd.rng[(i * jd.sample_size) + 3];
+
+		bool ok = lambdatwist_p4p<double, float, 5>((float*)&ja->p2d[i1], (float*)&ja->p2d[i2], (float*)&ja->p2d[i3], (float*)&ja->p2d[i4],
+			                                        (float*)&ja->p3d[i1], (float*)&ja->p3d[i2], (float*)&ja->p3d[i3], (float*)&ja->p3d[i4],
+			                                        ja->fx, ja->fy, ja->cx, ja->cy, R_temp, tvec_temp.val);
+
+		if (!ok) { continue; }
+
+		Rodrigues(cv::Matx33f((float*)R_temp), rvec_temp);
+
+		float t_sum = tvec_temp[0] + tvec_temp[1] + tvec_temp[2];
+		float r_sum = rvec_temp[0] + rvec_temp[1] + rvec_temp[2];
+
+		float x_sum = t_sum + r_sum;
+
+		if (!std::isfinite(x_sum)) { continue; }
+
+		int index = valid++;
+		
+		ja->out[6 * index + 0] = rvec_temp[0];
+		ja->out[6 * index + 1] = rvec_temp[1];
+		ja->out[6 * index + 2] = rvec_temp[2];
+		ja->out[6 * index + 3] = tvec_temp[0];
+		ja->out[6 * index + 4] = tvec_temp[1];
+		ja->out[6 * index + 5] = tvec_temp[2];
+	}
+}
+
+int batch_cpu_solver_p4p_lambdatwist(cv::Point2f const* p2d, cv::Point3f const* p3d, int point_count, cv::Mat const& K, int poses_to_sample, float* poses, int workers)
+{
+	job_arguments ja;
+
+	ja.p2d = p2d;
+	ja.p3d = p3d;
+	ja.fx = K.at<float>(0, 0);
+	ja.fy = K.at<float>(1, 1);
+	ja.cx = K.at<float>(0, 2);
+	ja.cy = K.at<float>(1, 2);
+	ja.out = poses;
+
+	int valid =  batch_solve(poses_to_sample, workers, block_cpu_solver_p4p_lambdatwist, &ja, point_count, 4);
+	//std::cout << "VALID: " << valid << std::endl;
+	return valid;
+}
+
+
+
+
+
+
+
+/*
 int batch_solve_lambdatwist_cpu(std::vector<cv::Point2f> const& pts2, std::vector<cv::Point3f> const& pts3, cv::Mat const& K, int poses_to_sample, cv::Mat& poses_pool)
 {
 	int n_points = (int)pts2.size();
@@ -44,3 +126,4 @@ int batch_solve_lambdatwist_cpu(std::vector<cv::Point2f> const& pts2, std::vecto
 
 	return poses_pool_used;
 }
+*/
