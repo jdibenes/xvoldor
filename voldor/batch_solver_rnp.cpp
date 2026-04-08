2aa38539a6f8cd9f8c79bb7204032d14a692bc16
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-04-07 20:50:45 -0400

    Update batch_solver_rnp.cpp

--- voldor/batch_solver_rnp.cpp
+++ voldor/batch_solver_rnp.cpp
@@ -1,11 +1,11 @@
 
-#include <opencv2/calib3d.hpp>
+#include "helpers_opencv.h"
 #include "batch_cpu_solver.h"
 #include "solvers.h"
 
-struct job_arguments
+struct job_inputs
 {
-	cv::Point2f const* p2d;
+	cv::Point2f const* p2k;
 	cv::Point3f const* p3d;
 	float fx;
 	float fy;
@@ -20,7 +20,7 @@ struct job_arguments
 
 static void block_cpu_solver_rnp(job_descriptor& jd)
 {
-	job_arguments* ja = static_cast<job_arguments*>(jd.inputs);
+	job_inputs* ji = static_cast<job_inputs*>(jd.inputs);
 
 	cv::Point2f p2d[7];
 	cv::Point3f p3d[7];
@@ -30,52 +30,63 @@ static void block_cpu_solver_rnp(job_descriptor& jd)
 
 	for (int i = jd.start; i < jd.end; ++i)
 	{
-		int const* p = get_sample_indices(jd, i);
+	int const* p = get_sample_indices(jd, i);
 
-		for (int m = 0; m < jd.sample_size; ++m)
-		{
-			p2d[m] = ja->p2d[p[m]];
-			p3d[m] = ja->p3d[p[m]];
+	for (int m = 0; m < jd.sample_size; ++m)
+	{
+	int im = p[m];
 
-			p2d[m].x = (p2d[m].x - ja->cx) / ja->fx;
-			p2d[m].y = (p2d[m].y - ja->cy) / ja->fy;
-		}
+	p3d[m] =            ji->p3d[im];
+	p2d[m] = p2k_to_p2d(ji->p2k[im], ji->fx, ji->fy, ji->cx, ji->cy);
+	}
 
-		bool ok;
+	bool ok;
 
-		switch (ja->solver)
-		{
-		case 1:  ok = solver_r6p2l(reinterpret_cast<float*>(p3d), reinterpret_cast<float*>(p2d), ja->direction, ja->r0, r, t);                     break;
-		case 2:  ok = solver_r6p2i(reinterpret_cast<float*>(p3d), reinterpret_cast<float*>(p2d), ja->direction, ja->r0, r, t, ja->max_iterations); break;
-		case 0:  
-		default: ok = solver_r6p1l(reinterpret_cast<float*>(p3d), reinterpret_cast<float*>(p2d), ja->direction, ja->r0, r, t);                     break;
-		}
+	switch (ji->solver)
+	{
+	case 0:  ok = solver_r6p1l(reinterpret_cast<float*>(p3d), reinterpret_cast<float*>(p2d), ji->direction, ji->r0, r, t);                     break;
+	case 1:  ok = solver_r6p2l(reinterpret_cast<float*>(p3d), reinterpret_cast<float*>(p2d), ji->direction, ji->r0, r, t);                     break;
+	case 2:  ok = solver_r6p2i(reinterpret_cast<float*>(p3d), reinterpret_cast<float*>(p2d), ji->direction, ji->r0, r, t, ji->max_iterations); break;
+	default: ok = false;                                                                                                                       break;
+	}
 
-		if (!ok) { continue; }
-		if (!is_valid_solution_6(r, t)) { continue; }
+	if (!ok) { continue; }
+	if (!is_valid_solution_6(r, t)) { continue; }
 
-		put_solution_6(jd, (float*)jd.output, r, t);
+	put_solution_6(jd, static_cast<float*>(jd.output), r, t);
 
-		jd.valid++;
+	jd.valid++;
 	}
 }
 
-int batch_cpu_solver_rnp(cv::Point2f const* p2d, cv::Point3f const* p3d, int point_count, int sample_size, cv::Mat const& K, int solver, bool direction, float r0, int max_pow, int max_iterations, int poses_to_sample, float* poses, int workers, bool unique)
+int batch_cpu_solver_rnp(cv::Point3f const* p3d_1, cv::Point2f const* p2k_2, int point_count, cv::Mat const& K, int solver, int poses_to_sample, float* poses, int workers, bool unique, bool direction, float r0, int max_pow, int max_iterations)
 {
-	job_arguments ja;
+	job_inputs ji;
 
-	ja.p2d = p2d;
-	ja.p3d = p3d;
-	ja.fx = K.at<float>(0, 0);
-	ja.fy = K.at<float>(1, 1);
-	ja.cx = K.at<float>(0, 2);
-	ja.cy = K.at<float>(1, 2);
-	ja.solver = solver;
-	ja.direction = direction;
-	ja.r0 = r0;
-	ja.max_pow = max_pow;
-	ja.max_iterations = max_iterations;
+	ji.p3d = p3d_1;
+	ji.p2k = p2k_2;	
+	ji.fx = K.at<float>(0, 0);
+	ji.fy = K.at<float>(1, 1);
+	ji.cx = K.at<float>(0, 2);
+	ji.cy = K.at<float>(1, 2);
+	ji.solver = solver;
+	ji.direction = direction;
+	ji.r0 = r0;
+	ji.max_pow = max_pow;
+	ji.max_iterations = max_iterations;
 
-	std::vector<job_result> jr = batch_solve(poses_to_sample, workers, block_cpu_solver_rnp, &ja, point_count, sample_size, unique, poses);
+	int sample_size;
+
+	switch (solver)
+	{
+	case 0:  sample_size = 7; break;
+	case 1:  sample_size = 7; break;
+	case 2:  sample_size = 6; break;
+	default: return 0;
+	}
+
+	if (point_count < sample_size) { return 0; }
+
+	std::vector<job_result> jr = batch_solve(poses_to_sample, workers, block_cpu_solver_rnp, &ji, point_count, sample_size, unique, poses);
 	return batch_finalize(jr, poses, 6);
 }
