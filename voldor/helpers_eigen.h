255a72b0cd5e9033e0e98c45be8f288d1d3df898
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-04-07 20:30:34 -0400

    Update helpers_eigen.h

--- voldor/helpers_eigen.h
+++ voldor/helpers_eigen.h
@@ -6,7 +6,7 @@
 
 // OK
 template <typename _scalar_out, int _rows, int _cols, typename _scalar_in>
-Eigen::Matrix<_scalar_out, _rows, _cols> matrix_from_buffer(_scalar_in const* data, int rows = _rows, int cols = _cols)
+Eigen::Matrix<_scalar_out, _rows, _cols> matrix_from_buffer(_scalar_in const* data, int rows = _rows, int cols = _cols) // default
 {
     Eigen::Map<Eigen::Matrix<_scalar_in, _rows, _cols> const> S(data, rows, cols);
     if constexpr (std::is_same_v<_scalar_out, _scalar_in>) { return S; } else { return S.cast<_scalar_out>(); }
