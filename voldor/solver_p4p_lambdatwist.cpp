528d5391c179a87ce6eceb5ea1c2ea2fa5ffbd8d
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-04-07 23:03:24 -0400

    p4p solvers refactor

--- voldor/solver_p4p_lambdatwist.cpp
+++ voldor/solver_p4p_lambdatwist.cpp
@@ -5,14 +5,14 @@
 #include "helpers_eigen.h"
 #include "helpers_geometry.h"
 
-bool solver_p4p_lambdatwist(float* p3d_1, float* p2k_2, float fx, float fy, float cx, float cy, float* r_12, float* t_12)
+bool solver_p4p_lambdatwist(float* p3d_1, float* p2d_2, float* r_12, float* t_12)
 {
 	float R_s[3][3];
 
 	cv::Vec3f r_s;
 	cv::Vec3f t_s;
 
-	bool ok = lambdatwist_p4p<double, float, 5>(&p2k_2[0], &p2k_2[1], &p2k_2[2], &p2k_2[3], &p3d_1[0], &p3d_1[1], &p3d_1[2], &p3d_1[3], fx, fy, cx, cy, R_s, t_s.val);
+	bool ok = lambdatwist_p4p<double, float, 5>(&p2d_2[0], &p2d_2[1], &p2d_2[2], &p2d_2[3], &p3d_1[0], &p3d_1[1], &p3d_1[2], &p3d_1[3], 1, 1, 0, 0, R_s, t_s.val);
 	if (!ok) { return false; }
 
 	cv::Rodrigues(cv::Matx33f(reinterpret_cast<float*>(R_s)), r_s);
