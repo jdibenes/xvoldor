346e2acfda735d9a2721000b8eb4525268d0a0a9
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-04-08 01:14:38 -0400

    Update solver_p4p_lambdatwist.cpp

--- voldor/solver_p4p_lambdatwist.cpp
+++ voldor/solver_p4p_lambdatwist.cpp
@@ -7,12 +7,15 @@
 
 bool solver_p4p_lambdatwist(float* p3d_1, float* p2d_2, float* r_12, float* t_12)
 {
+	cv::Vec3f* p1 = reinterpret_cast<cv::Vec3f*>(p3d_1);
+	cv::Vec2f* p2 = reinterpret_cast<cv::Vec2f*>(p2d_2);
+
 	float R_s[3][3];
 
 	cv::Vec3f r_s;
 	cv::Vec3f t_s;
 
-	bool ok = lambdatwist_p4p<double, float, 5>(&p2d_2[0], &p2d_2[1], &p2d_2[2], &p2d_2[3], &p3d_1[0], &p3d_1[1], &p3d_1[2], &p3d_1[3], 1, 1, 0, 0, R_s, t_s.val);
+	bool ok = lambdatwist_p4p<double, float, 5>(p2[0].val, p2[1].val, p2[2].val, p2[3].val, p1[0].val, p1[1].val, p1[2].val, p1[3].val, 1, 1, 0, 0, R_s, t_s.val);
 	if (!ok) { return false; }
 
 	cv::Rodrigues(cv::Matx33f(reinterpret_cast<float*>(R_s)), r_s);
