14e46f6fd761751cddf800d9126423330b98f148
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-04-08 01:26:09 -0400

    Update solver_p4p_ap3p.cpp

--- voldor/solver_p4p_ap3p.cpp
+++ voldor/solver_p4p_ap3p.cpp
@@ -4,12 +4,12 @@
 #include "helpers_eigen.h"
 #include "helpers_geometry.h"
 
-bool solver_p4p_ap3p(float* p3d_1, float* p2d_2, float* r_12, float* t_12)
+bool solver_p4p_ap3p(float const* p3d_1, float const* p2d_2, float* r_12, float* t_12)
 {
 	cv::Vec3d r_s;
 	cv::Vec3d t_s;
 
-	bool ok = solvePnP(cv::_InputArray(reinterpret_cast<cv::Point3f*>(p3d_1), 4), cv::_InputArray(reinterpret_cast<cv::Point2f*>(p2d_2), 4), cv::Mat::eye(3, 3, CV_32F), cv::Mat(), r_s, t_s, false, cv::SOLVEPNP_AP3P);
+	bool ok = solvePnP(cv::_InputArray(reinterpret_cast<cv::Point3f const*>(p3d_1), 4), cv::_InputArray(reinterpret_cast<cv::Point2f const*>(p2d_2), 4), cv::Mat::eye(3, 3, CV_32F), cv::Mat(), r_s, t_s, false, cv::SOLVEPNP_AP3P);
 	if (!ok) { return false; }
 
 	Eigen::Matrix<double, 3, 1> r = matrix_from_buffer<double, 3, 1>(r_s.val);
