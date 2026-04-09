67187faaf4960a3730f65ebb1266eb961b628c2e
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-04-09 19:11:38 -0400

    Update main.cpp

--- voldor/main.cpp
+++ voldor/main.cpp
@@ -2,18 +2,36 @@
 #define _CRT_SECURE_NO_WARNINGS
 #endif
 
-
+#include "helpers_eigen.h"
+#include "helpers_geometry.h"
 #include "voldor_utils.h"
 #include "config.h"
 #include "geometry.h"
 #include "voldor.h"
 #include "../gpu-kernels/gpu_kernels.h"
 #include "py_export.h"
+#include "lock.h"
 
 using namespace cv;
 using namespace std;
 
 void load_file(char const* filename, void* buffer, int offset, int count);
+
+Eigen::Matrix<float, 4, 4> load_pose(char const* filename)
+{
+	FILE* f = fopen(filename, "rb");
+	if (f == NULL) { throw std::runtime_error(""); }
+	Cleaner file_close([=]() { fclose(f); });
+
+	Eigen::Matrix<float, 4, 4> pose;
+	uint32_t total = 4 * 4;
+	uint32_t count = fread(pose.data(), sizeof(float), total, f);
+	if (count != total) { throw std::runtime_error(""); }
+
+	pose.transposeInPlace();
+
+	return pose;
+}
 
 
 int main(int argc, char* argv[]) {
@@ -74,6 +92,30 @@ int main(int argc, char* argv[]) {
 	load_file("C:/Users/jcds/Documents/GitHub/xvoldor/demo/data/hl2_5/disp_gt/000065.flo", disparities_pt + 4 * (w * h * 2), 12, -1);
 	load_file("C:/Users/jcds/Documents/GitHub/xvoldor/demo/data/hl2_5/disp_gt/000066.flo", disparities_pt + 5 * (w * h * 2), 12, -1);
 
+	// hl2_to_opencv = np.array([[1,0,0,0],[0,-1,0,0],[0,0,-1,0],[0,0,0,1]], dtype=np.float32)
+	Eigen::Matrix<float, 4, 4> hl2_to_opencv{
+		{1,  0,  0,  0},
+		{0, -1,  0,  0},
+		{0,  0, -1,  0},
+		{0,  0,  0,  1}
+	};
+
+
+	Eigen::Matrix<float, 4, 4> a_pose_1 = hl2_to_opencv * load_pose("C:/Users/jcds/Documents/GitHub/xvoldor/demo/data/hl2_5/pose/000061.bin").transpose() * hl2_to_opencv;
+	Eigen::Matrix<float, 4, 4> a_pose_2 = hl2_to_opencv * load_pose("C:/Users/jcds/Documents/GitHub/xvoldor/demo/data/hl2_5/pose/000062.bin").transpose() * hl2_to_opencv;
+	Eigen::Matrix<float, 4, 4> a_pose_3 = hl2_to_opencv * load_pose("C:/Users/jcds/Documents/GitHub/xvoldor/demo/data/hl2_5/pose/000063.bin").transpose() * hl2_to_opencv;
+	Eigen::Matrix<float, 4, 4> a_pose_4 = hl2_to_opencv * load_pose("C:/Users/jcds/Documents/GitHub/xvoldor/demo/data/hl2_5/pose/000064.bin").transpose() * hl2_to_opencv;
+	Eigen::Matrix<float, 4, 4> a_pose_5 = hl2_to_opencv * load_pose("C:/Users/jcds/Documents/GitHub/xvoldor/demo/data/hl2_5/pose/000065.bin").transpose() * hl2_to_opencv;
+	Eigen::Matrix<float, 4, 4> a_pose_6 = hl2_to_opencv * load_pose("C:/Users/jcds/Documents/GitHub/xvoldor/demo/data/hl2_5/pose/000066.bin").transpose() * hl2_to_opencv;
+
+	Eigen::Matrix<float, 4, 4> r_pose[] = {
+		a_pose_2.inverse() * a_pose_1,
+		a_pose_3.inverse() * a_pose_2,
+		a_pose_4.inverse() * a_pose_3,
+		a_pose_5.inverse() * a_pose_4,
+		a_pose_6.inverse() * a_pose_5
+	};
+
 	for (int i = 0; i < (w * h); ++i)
 	{
 		disparity_pt[i] = -disparity_pt[2 * i];
@@ -114,8 +156,18 @@ int main(int argc, char* argv[]) {
 			std::cout << poses[6 * i + j];
 			if (j < 5) { std::cout << ", "; }
 		}
+		Eigen::Matrix<float, 3, 3> R_gt = r_pose[i](Eigen::seqN(0, 3), Eigen::seqN(0, 3));
+		Eigen::Matrix<float, 3, 1> r_gt = vector_r_rodrigues(R_gt);
+		Eigen::Matrix<float, 3, 1> t_gt = r_pose[i](Eigen::seqN(0, 3), 3);
+		//std::cout << " | r_gt: " << r_gt << " | t_gt: " << t_gt;
+		Eigen::Matrix<float, 2, 1> errors = compute_error(r_gt, t_gt, matrix_from_buffer<float, 3, 1>(poses + 6 * i), matrix_from_buffer<float, 3, 1>(poses + 6 * i + 3));
+		std::cout << " | Errors: " << errors(0) << ", " << errors(1);
 		std::cout << std::endl;
 	}
+
+
+	char c;
+	std::cin >> c;
 
 	return 0;
 }
