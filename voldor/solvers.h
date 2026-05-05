8668f5818c4158d16be6cc9a08ee364094421313
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-05-04 23:20:23 -0400

    hpc3 solver

--- voldor/solvers.h
+++ voldor/solvers.h
@@ -4,6 +4,7 @@
 bool solver_gpm_hpc0(float const* p3d_1, float const* p3d_2, float* r_12, float* t_12);
 bool solver_gpm_hpc1(float const* p3d_1, float const* p3d_2, float* r_12, float* t_12);
 bool solver_gpm_hpc2(float const* p3d_1, float const* p3d_2, float* r_12, float* t_12);
+bool solver_gpm_hpc3(float const* p3d_1, float const* p3d_2, float* r_12, float* t_12);
 
 bool solver_gpm_nm5(float const* p3d_1, float const* p2h_2, float* r_12, float* t_12);
 bool solver_gpm_nm6(float const* p3d_1, float const* p2h_2, float* r_12, float* t_12);
