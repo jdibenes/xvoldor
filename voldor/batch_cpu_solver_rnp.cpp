99ad5235ad6d7715964a2b98ae0cb6ab378c73bf
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-05-18 23:16:04 -0400

    rnp names

--- voldor/batch_cpu_solver_rnp.cpp
+++ voldor/batch_cpu_solver_rnp.cpp
@@ -53,10 +53,10 @@ static void block_cpu_solver_rnp(job_descriptor& jd)
 
 	switch (ji->solver)
 	{
-	case 0:  ok = solver_r6p1l(reinterpret_cast<float*>(p3d_1), reinterpret_cast<float*>(p2d_2), ji->direction, ji->r0, r, t, dr, dt);                     break;
-	case 1:  ok = solver_r6p2l(reinterpret_cast<float*>(p3d_1), reinterpret_cast<float*>(p2d_2), ji->direction, ji->r0, r, t, dr, dt);                     break;
-	case 2:  ok = solver_r6p2i(reinterpret_cast<float*>(p3d_1), reinterpret_cast<float*>(p2d_2), ji->direction, ji->r0, r, t, dr, dt, ji->max_iterations); break;
-	default: ok = false;                                                                                                                                   break;
+	case 0:  ok = solver_rnp_r6p1l(reinterpret_cast<float*>(p3d_1), reinterpret_cast<float*>(p2d_2), ji->direction, ji->r0, r, t, dr, dt);                     break;
+	case 1:  ok = solver_rnp_r6p2l(reinterpret_cast<float*>(p3d_1), reinterpret_cast<float*>(p2d_2), ji->direction, ji->r0, r, t, dr, dt);                     break;
+	case 2:  ok = solver_rnp_r6p2i(reinterpret_cast<float*>(p3d_1), reinterpret_cast<float*>(p2d_2), ji->direction, ji->r0, r, t, dr, dt, ji->max_iterations); break;
+	default: ok = false;                                                                                                                                       break;
 	}
 
 	if (!ok) { continue; }
