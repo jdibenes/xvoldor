ac6dad604788ac23c8b8d88423227f281fd8bd2b
jdibenes <58836755+jdibenes@users.noreply.github.com>
2026-05-28 03:11:13 -0400

    Update helpers_file.cpp

--- voldor/helpers_file.cpp
+++ voldor/helpers_file.cpp
@@ -1,4 +1,8 @@
 
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
 #include <stdio.h>
 #include <string>
 #include <stdexcept>
@@ -18,7 +22,7 @@ void load_file(char const* filename, void* buffer, int offset, int count)
         bytes = count;
     }
     fseek(f, offset, SEEK_SET);
-    int bytes_read = fread(buffer, 1, bytes, f);
+    int bytes_read = static_cast<int>(fread(buffer, 1, bytes, f));
     fclose(f);
     if (bytes_read != bytes) { throw std::runtime_error("Read " + std::to_string(bytes_read) + "/" + std::to_string(bytes) + " from file: " + filename); }
 }
